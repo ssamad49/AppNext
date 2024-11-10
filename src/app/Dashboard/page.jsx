ebd55fe463096d980ff3e5d26e0040b5fd404351
Rochdi <ro@Rochdis-MacBook-Pro.local>
2024-11-10 15:02:06 +0100

    fix metadata

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,5 +1,12 @@
 import FormLogin from "@/components/formLogin";
 
+export const metadata = {
+    title: {
+      default: "Dashboard",
+    },
+    description: "Dashboard",
+  };
+
 export default function dashboard() {
 
     return (
